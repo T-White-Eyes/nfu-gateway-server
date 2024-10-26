327a70da6f69835b39b322d0361c246b79f0f69a
qoxogus <a01034367055@gmail.com>
2024-10-26 21:25:31 +0900

    feat: @ConfigurationPropertiesScan 추가

--- src/main/kotlin/com/nfu/gateway/GatewayApplication.kt
+++ src/main/kotlin/com/nfu/gateway/GatewayApplication.kt
@@ -1,11 +1,13 @@
 package com.nfu.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class GatewayApplication
 
 fun main(args: Array<String>) {
